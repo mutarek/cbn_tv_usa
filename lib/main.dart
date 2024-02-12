6ef49c3efba803aeb82e54d7ab3a6500840c7295
mutarek <mohiuddintarek2017@gmail.com>
2024-02-12 09:57:16 +0600

    updated version

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo apps',
+      title: 'CBN TV USA',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
