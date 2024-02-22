ee14fe1d172b855aafaa6397a6945ea7d68ec9c3
mutarek <mohiuddintarek2017@gmail.com>
2024-02-23 00:59:04 +0600

    search vanish

--- lib/app/module/home/screen/home_page.dart
+++ lib/app/module/home/screen/home_page.dart
@@ -40,110 +40,29 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.all(12),
                 child: Column(
                   children: [
-                    _showAppbar
-                        ? Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                border: Border.all(color: primaryColor)),
-                            height: 50,
-                            child: TextField(
-                              onChanged: (value) {
-                                controller.searchFromList(value);
-                              },
-                              decoration: const InputDecoration(
-                                  contentPadding: EdgeInsets.only(left: 10),
-                                  hintText: "Search News",
-                                  suffixIcon: Icon(Icons.search),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(8.0)))),
-                            ),
-                          )
-                        : const SizedBox.shrink(),
+                    // _showAppbar
+                    //     ? Container(
+                    //         decoration: BoxDecoration(
+                    //             borderRadius: BorderRadius.circular(15),
+                    //             border: Border.all(color: primaryColor)),
+                    //         height: 50,
+                    //         child: TextField(
+                    //           onChanged: (value) {
+                    //             controller.searchFromList(value);
+                    //           },
+                    //           decoration: const InputDecoration(
+                    //               contentPadding: EdgeInsets.only(left: 10),
+                    //               hintText: "Search News",
+                    //               suffixIcon: Icon(Icons.search),
+                    //               border: OutlineInputBorder(
+                    //                   borderRadius: BorderRadius.all(
+                    //                       Radius.circular(8.0)))),
+                    //         ),
+                    //       )
+                    //     : const SizedBox.shrink(),
                     const SizedBox(height: 10),
                     Expanded(
-                      child: controller.searchModelList.isNotEmpty
-                          ? ListView.builder(
-                              shrinkWrap: true,
-                              controller: _scrollViewController,
-                              physics: const BouncingScrollPhysics(),
-                              itemCount: controller.searchModelList.length,
-                              itemBuilder: (_, index) {
-                                if (index ==
-                                    controller.postModelList.length - 3) {
-                                  controller.getNextPage();
-                                }
-                                var data = controller.searchModelList[index];
-                                return InkWell(
-                                  onTap: () {
-                                    controller.updatePostIndex(index);
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (builder) =>
-                                                const DetailPostScreen()));
-                                  },
-                                  child: Card(
-                                    elevation: 0.7,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(15),
-                                      ),
-                                      margin: const EdgeInsets.all(7),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8),
-                                            child: CachedNetworkImage(
-                                              height: height * 0.2,
-                                              imageUrl: (data.yoast_head_json
-                                                      ?.ogImage[0].url ??
-                                                  ""),
-                                              imageBuilder:
-                                                  (context, imageProvider) =>
-                                                      Container(
-                                                decoration: BoxDecoration(
-                                                  image: DecorationImage(
-                                                    image: imageProvider,
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                ),
-                                              ),
-                                              placeholder: (context, url) =>
-                                                  const LoaddingShimmer(
-                                                height: 150,
-                                                weight: double.infinity,
-                                              ),
-                                              errorWidget:
-                                                  (context, url, error) =>
-                                                      const Icon(Icons.error),
-                                            ),
-                                          ),
-                                          const SizedBox(height: 5),
-                                          Text(data.title?.rendered ?? "",
-                                              style: const TextStyle(
-                                                fontWeight: FontWeight.w600,
-                                                fontSize: 16,
-                                              )),
-                                          const SizedBox(height: 5),
-                                          Text(
-                                              "প্রকাশিত : ${data.date?.day ?? 0} ${controller.enMonthtoBdMonth(data.date?.month ?? 1)} ${controller.enMonthtoBdMonth(data.date?.year ?? 1)}"),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            )
-                          : ListView.builder(
+                      child: ListView.builder(
                               shrinkWrap: true,
                               controller: _scrollViewController,
                               physics: const BouncingScrollPhysics(),
