552d117670c0386aff1658317c949ea443013cf9
mutarek <mohiuddintarek2017@gmail.com>
2024-03-14 11:35:32 +0600

    added videos

--- lib/app/module/home/screen/live_tv_screen.dart
+++ lib/app/module/home/screen/live_tv_screen.dart
@@ -72,7 +72,7 @@ class _LiveTVScreenState extends State<LiveTVScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Recent Video News",style: TextStyle(
+                Text("Recent Video News ",style: TextStyle(
                     fontWeight: FontWeight.w600,
                     fontSize: 16
                 )),
